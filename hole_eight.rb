b86e45b09f94cc4e67204077a6683cc3f2ba969e
Andrew Locke <alocke12992@gmail.com>
2018-02-19 17:30:45 -0700

    completes hole eight

---
+++ hole_eight.rb
@@ -0,0 +1,17 @@
+#HOLE 8: Love Test
+# # Create a method that takes in 2 strings 
+# and counts the total number of characters 
+# in common. Divide the total number of chars 
+# by the number in common. Spaces do not count. 
+#   Capitols are not the same as lowercase 
+#   Example: "I love this code", "This code loves me" Total Chars: 27 Chars In Common: 7 Solution: 27 / 7 = 3
+
+def lov(x,y)
+  t = x.length + y.length 
+  c = (x.chars & y.chars).size
+  s = t / s
+
+end 
+
+lov("I love this code", "This code loves me")
+
